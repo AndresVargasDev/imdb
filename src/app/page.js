d672d12456fcec494e1e7fc4197fa8855c22fbb9
akanashimi <andresvargas094@gmail.com>
2023-03-23 20:44:27 -0600

    create the about

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <div>
+      <h1 className="text-2xl font-medium text-amber-600">Home</h1>
+    </div>
+  );
 }
