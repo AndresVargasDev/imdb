77d72d922dcfe4e6eb971861abd2a8b7993a5ebe
akanashimi <andresvargas094@gmail.com>
2023-03-24 20:01:08 -0600

    card by movie id

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import SearchBox from "../components/SearchBox";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           {/* NavBar  */}
           <Navbar />
           {/* SearchBox */}
+          <SearchBox />
           {children}
         </Providers>
       </body>
