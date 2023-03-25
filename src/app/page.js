4c3b691105ce2a8c676a5dd637be4eac8ea9078f
akanashimi <andresvargas094@gmail.com>
2023-03-24 20:14:03 -0600

    fix the fetching

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import Results from "../components/Results";
 
 const API_KEY = process.env.API_KEY;
 
+export const dynamic = "force-dynamic";
+
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    { nex: { revalidate: 10000 } }
+    { next: { revalidate: 10000 } }
   );
 
   if (!res.ok) {
